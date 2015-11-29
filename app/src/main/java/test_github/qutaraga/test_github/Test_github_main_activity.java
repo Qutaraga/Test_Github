210e3eafabcef2103740e944930802822f1f5061
Qutaraga <qutaraga@gmail.com>
2015-11-29 23:18:14 +0300

    Create base project and send him on Github.

---
+++ app/src/main/java/test_github/qutaraga/test_github/Test_github_main_activity.java
@@ -0,0 +1,13 @@
+package test_github.qutaraga.test_github;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test_github_main_activity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate((savedInstanceState));
+        setContentView(R.layout.activity_main);
+    }
+}
